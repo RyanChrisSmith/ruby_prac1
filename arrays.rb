3a3cec2d18e19c4102ba3b0776b810be3b8aba06
Ryan Smith <ryan.chris.smith@gmail.com>
2022-06-08 10:00:32 -0600

    Add methods and explanations

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ neighbs = ["Kelsey","Kamil","Carl","Erika"]
 feet = [1,4,7,3]
 payments = [22.11,4.20,6.49,444.80]
 winning = [true,false,true,true]
+
+# This method removes the last element of the array
+neighbs.pop
+
+# This mehod adds the element to the end of the array
+feet.push(17)
+
+# This method removes the first element of the array
+payments.shift
+
+# This method adds to the beginning of the array
+winning.unshift(false)
